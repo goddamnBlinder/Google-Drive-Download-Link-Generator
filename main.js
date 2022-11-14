6723851bfaf01bc87da1d9c137bbe0477d4d8c21
James Chinweike <chinweikechukwudi@gmail.com>
2022-11-14 06:10:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 const glink = document.querySelector("#glink");
 const btn = document.querySelector("#btn");
+const downloadLink_textArea = document.querySelector("#download-link");
 
 btn.addEventListener('click', (e) => {
       e.preventDefault();
-      console.log("entered");
+    //   console.log(glink.value);
+
+    const glinkValued = glink.value
+    const confirm = glink.value.includes("https://drive.google.com/file/d/");
+    // console.log(glink.value);
+
+    if(confirm == true){
+       var getDownloadLink = glink.value
+       .replace("https://drive.google.com/file/d/", "https://drive.google.com/uc?export=download&id=")
+       .replace("view?usp=share_link", " ");
+
+    //    console.log(getDownloadLink);
+     downloadLink_textArea.textContent = getDownloadLink;
+
+    }
+
+
 });
+
+let copied = document.querySelector(".copy");
