5aa78039e862d04f02277c8a7d54d6aa91ab0fed
James Chinweike <chinweikechukwudi@gmail.com>
2022-11-14 05:30:00 +0100

    Update main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const glink = document.querySelector("#glink");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      console.log("entered");
+});
